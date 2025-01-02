4c4ef27790a35004fd0da8a0c6fdbf5f095706e6
Cao Hoàng Khánh Băng <caohoangkhanhbang2003@gmail.com>
2025-01-02 12:10:07 +0700

    buil layouts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Following from './pages/Following'
+import { publicRoutes } from './routes';
+// import DefaultLayout from './components/Layout/DefaultLayout';
+import { DefaultLayout } from './components/Layout'
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Cách 2 link qua thư mục routes */}
+          {publicRoutes.map((page, index) => {
+            let Layout = page.layout === null ? Fragment : page.layout ? page.layout : DefaultLayout;
+            console.log(Layout);
+
+
+            const Page = page.component;
+            return (< Route key={index} path={page.path} element={
+              <Layout>
+                <Page />
+              </Layout>
+            } />)
+          })}
+
+
+          {/* Cách 1 để trực tiếp tại đây */}
+          {/* <Route path='/' element={<Home />}></Route>
+          <Route path='/following' element={<Following />}></Route> */}
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
