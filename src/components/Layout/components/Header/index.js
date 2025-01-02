d2d5ef90ea60d090e0ac27bd7c06bda62a505097
Cao Hoàng Khánh Băng <caohoangkhanhbang2003@gmail.com>
2025-01-02 21:02:05 +0700

    Dựng khung layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,16 @@
+import style from './Header.module.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(style)
+
 function Header() {
-    return <h1>Header</h1>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+
+            </div>
+        </header>
+    )
 }
 
 export default Header
