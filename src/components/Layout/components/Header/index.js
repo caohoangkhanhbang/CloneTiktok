1ad9c9abe72985fb6c0b330729a29416ef3bdd29
Cao Hoàng Khánh Băng <caohoangkhanhbang2003@gmail.com>
2025-01-03 21:07:19 +0700

    Thêm phần header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,35 @@
 import style from './Header.module.scss'
 import classNames from 'classnames/bind'
+import images from '../../../../asset/images/logo.svg'
+import img2 from '../../../../asset/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const cx = classNames.bind(style)
+console.log(img2.logo, img2.default);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images} alt='logo tiktok' />
+                    {/* <img src={img2.logo} alt='tiktok cách 2'/> */}
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder='Search account and video' spellCheck='false' />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
 
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}> 
+                    
+                </div>
             </div>
         </header>
     )
